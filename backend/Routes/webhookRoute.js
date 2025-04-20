e8de6b710883a9b38c69abcb65dc4e327977afd9
shuyi320 <shuyi021@gmail.com>
2025-04-20 01:04:53 -0400

    Add webhook route for handling webhooks from Clerk

---
+++ backend/Routes/webhookRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { Webhook } from "../ Controllers / webhookController.js";
+
+const router = express.Router();
+router.use(express.raw({ type: 'application/json' }));
+
+router.post('/', Webhook);
+
+export default router;
--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 const app = express();
 import { sequelize } from "./Models/db.js";
 import furnitureRoutes from "./Routes/furnitureRoutes.js";
+import webhookRoutes from "./Routes/webhookRoute.js";
 
 
 // CORS configuration allowing both localhost and 127.0.0.1
@@ -19,6 +20,7 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.use("/api/furnitures", furnitureRoutes);
+app.use("/api/webhook", webhookRoutes);
 
 // start database
 await sequelize.sync({ force: false });
